af2e7077be2e43b0813d6c47cc077ad71703eb78
Ooi Chia Ying <chia.ying@masverse.com.my>
2024-07-18 17:47:10 +0800

    Chnage connection

--- app/api/actions/donate/route.ts
+++ app/api/actions/donate/route.ts
@@ -106,7 +106,7 @@ export const POST = async (req: Request) => {
       });
     }
 
-    const connection = new Connection(DEFAULT_RPC);
+    const connection = new Connection('https://api.devnet.solana.com');
 
     // ensure the receiving account will be rent exempt
     const minimumBalance = await connection.getMinimumBalanceForRentExemption(
